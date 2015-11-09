1e662737dd2563465e4cfdffdefbf61e79d6727e
Jonathan Baltazar <jonrules@gmail.com>
2015-11-09 15:21:11 -0600

    add settings page

---
+++ templates/admin/wc-autoship-custom-meta-fields.php
@@ -0,0 +1,20 @@
+<h4>Custom Meta Fields</h4>
+
+<table class="wc-autoship-custom-meta-fields-table">
+	<thead>
+	<tr>
+		<th><?php echo __( 'Meta Key', 'wc-autoship-custom-meta' ); ?></th>
+		<th><?php echo __( 'Default Value', 'wc-autoship-custom-meta' ); ?></th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php for ( $row = 0; $row < 10; $row++ ): ?>
+		<?php $key = isset( $fields[ $row ]['key'] ) ? $fields[ $row ]['key'] : ''; ?>
+		<?php $default_value = isset( $fields[ $row ]['default_value'] ) ? $fields[ $row ]['default_value'] : ''; ?>
+		<tr>
+			<td><input type="text" name="wc_autoship_custom_meta_fields[<?php echo $row; ?>][key]" class="wc-autoship-custom-meta-field-key" value="<?php echo esc_attr( $key ); ?>" /></td>
+			<td><input type="text" name="wc_autoship_custom_meta_fields[<?php echo $row; ?>][default_value]" class="wc-autoship-custom-meta-field-default-value" value="<?php echo esc_attr( $default_value ); ?>" /></td>
+		</tr>
+	<?php endfor; ?>
+	</tbody>
+</table>
--- woocommerce-autoship-custom-meta.php
+++ woocommerce-autoship-custom-meta.php
@@ -24,3 +24,51 @@ function wc_autoship_custom_meta_uninstall() {
 
 }
 register_uninstall_hook( __FILE__, 'wc_autoship_custom_meta_uninstall' );
+
+function wc_autoship_custom_meta_settings( $settings ) {
+	$settings[] = array(
+		'title' => __( 'Custom Meta', 'wc-autoship-custom-meta' ),
+		'desc' => __( 'Add custom meta fields to autoship orders', 'wc-autoship-custom-meta' ),
+		'desc_tip' => false,
+		'type' => 'title',
+		'id' => 'wc_autoship_product_page_title'
+	);
+	$settings[] = array(
+		'name' => __( 'Custom Meta Fields', 'wc-autoship-custom-meta' ),
+		'desc' => __( 'The meta fields to show for autoship orders', 'wc-autoship-custom-meta' ),
+		'desc_tip' => true,
+		'type' => 'wc_autoship_custom_meta_fields',
+		'id' => 'wc_autoship_custom_meta_fields_tmp'
+	);
+	$settings[] = array(
+		'type' => 'sectionend',
+		'id' => 'wc_autoship_custom_meta_sectionend'
+	);
+	return $settings;
+}
+add_filter( 'wc_autoship_settings', 'wc_autoship_custom_meta_settings', 10, 1 );
+
+function wc_autoship_custom_meta_fields( $value ) {
+	$fields = get_option( 'wc_autoship_custom_meta_fields', array() );
+	include( 'templates/admin/wc-autoship-custom-meta-fields.php' );
+}
+add_action( 'woocommerce_admin_field_wc_autoship_custom_meta_fields', 'wc_autoship_custom_meta_fields' );
+
+function wc_autoship_custom_meta_update_options( $options ) {
+	if ( isset( $_POST['wc_autoship_custom_meta_fields'] ) ) {
+		$fields = $_POST['wc_autoship_custom_meta_fields'];
+		$filtered_fields = array();
+		foreach ( $fields as $field ) {
+			if ( ! empty( $field['key'] ) ) {
+				$filtered_fields[] = $field;
+			}
+		}
+		usort( $filtered_fields, 'wc_autoship_custom_meta_key_compare' );
+		update_option( 'wc_autoship_custom_meta_fields', $filtered_fields );
+	}
+}
+add_action( 'woocommerce_update_options_wc_autoship', 'wc_autoship_custom_meta_update_options' );
+
+function wc_autoship_custom_meta_key_compare( $a, $b ) {
+	return strcasecmp( $a['key'], $b['key'] );
+}
